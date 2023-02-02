115039cfe3e7aeb3815bf42ca0e2d32237d841cb
Your Name <you@example.com>
2023-02-02 17:35:26 +0545

    new

--- Week3/src/Day3/IfStatement.java
+++ Week3/src/Day3/IfStatement.java
@@ -7,112 +7,110 @@ public class IfStatement {
 		  System.out.println("Two"); // if(bn==3) // System.out.println("Three")
 		  //byte bn = 10; //if(bn!=0) //System.out.println("Non zero");
 		 */	
-//		example 3
+		//example 3
 
-//		byte bn =8; 
-//		if(bn!=0) System.out.println("Non"); 
-//		System.out.println("zero");
-//		System.out.println("Bye!");
+// 		byte bn =8; 
+// 		if(bn!=0) System.out.println("Non"); 
+// 		System.out.println("zero");
+// 		System.out.println("Bye!");
 		
-//		byte bn = 0;
-//		if (bn==0)
-//			System.out.println("Zero");
-//		else
-//			System.out.println("Not Zero");
+// 		byte bn = 0;
+// 		if (bn==0)
+// 			System.out.println("Zero");
+// 		else
+// 			System.out.println("Not Zero");
 		
-//		example=5
-//		byte bn = 5;
-//		if (bn==0) {
-//			System.out.println("zero");
-//			
-//		}
-//		else if(bn==1) {
-//			System.out.println("One");
-//		}
-//			else if(bn==2) {
-//				System.out.println("Two");
-//			}
-//			else if(bn==3) {
-//				System.out.println("three");
-//			}
-//			else if(bn==4) {
-//				System.out.println("Four");
-//			}
-//			else if(bn==5) {
-//				System.out.println("Five");
-//			}
-//			else if(bn==6) {
-//				System.out.println("Six");
-//			}
-//			else if(bn==7) {
-//				System.out.println("Seven");
-//			}
-//			else if(bn==8) {
-//				System.out.println("Eight");
-//			}
-//			else 
-//		System.out.println("Nine");
+// 		example=5
+// 		byte bn = 5;
+// 		if (bn==0) {
+// 			System.out.println("zero");
+			
+// 		}
+// 		else if(bn==1) {
+// 			System.out.println("One");
+// 		}
+// 			else if(bn==2) {
+// 				System.out.println("Two");
+// 			}
+// 			else if(bn==3) {
+// 				System.out.println("three");
+// 			}
+// 			else if(bn==4) {
+// 				System.out.println("Four");
+// 			}
+// 			else if(bn==5) {
+// 				System.out.println("Five");
+// 			}
+// 			else if(bn==6) {
+// 				System.out.println("Six");
+// 			}
+// 			else if(bn==7) {
+// 				System.out.println("Seven");
+// 			}
+// 			else if(bn==8) {
+// 				System.out.println("Eight");
+// 			}
+// 			else 
+// 		System.out.println("Nine");
 		
-//example 6
+// example 6
 		
-//		byte n1=7, n2=8,n3=9,n4=0;
-//		if(n1>n2)
-//			if(n1>n3)
-//				n4=n1;
-//		if(n2>n1)
-//			if(n2>n3)
-//				n4=n2;
-//		if(n2>n1)
-//			if(n3>n2)
-//				n4=n3;
-//		System.out.println("Result: "+n4);
-//		
-//		  byte n1=9, n2=8,n3=3,n4=0;
-//		  if((n1>n2)&& (n1>n3))
-//				  n4=n1;
-//		  if((n2>n1)&& (n2>n3))
-//			  n4=n2;
-//		  if((n3>n1)&& (n3>n2))
-//			  n4=n3;
-//		  System.out.println("largest no: "+n4);
+// 		byte n1=7, n2=8,n3=9,n4=0;
+// 		if(n1>n2)
+// 			if(n1>n3)
+// 				n4=n1;
+// 		if(n2>n1)
+// 			if(n2>n3)
+// 				n4=n2;
+// 		if(n2>n1)
+// 			if(n3>n2)
+// 				n4=n3;
+// 		System.out.println("Result: "+n4);
 		
-//		byte sub1 = 75;
-//		byte sub2 = 56;
-//		final byte PM= 40;
-//		
-//		if(sub1>=PM)
-//			if (sub2>=PM)
-//			System.out.println("PASS");
-//		else 
-//			System.out.println("FAIL");
+// 		  byte n1=9, n2=8,n3=3,n4=0;
+// 		  if((n1>n2)&& (n1>n3))
+// 				  n4=n1;
+// 		  if((n2>n1)&& (n2>n3))
+// 			  n4=n2;
+// 		  if((n3>n1)&& (n3>n2))
+// 			  n4=n3;
+// 		  System.out.println("largest no: "+n4);
+		
+// 		byte sub1 = 75;
+// 		byte sub2 = 56;
+// 		final byte PM= 40;
+		
+// 		if(sub1>=PM)
+// 			if (sub2>=PM)
+// 			System.out.println("PASS");
+// 		else 
+// 			System.out.println("FAIL");
 	
-		// byte start=1;
-//		byte stop =5;
-//		while(start<=stop) {
-//			System.out.println("PCPS COLLEGE");
-//			start ++;
-//		}
-//		
-//		 
-//		 System.out.println("pcps");
-//		 System.out.println("pcps");
-//		 System.out.println("pcps");
-//		 System.out.println("pcps");
-//		 System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps"
+// 		byte start=1;
+// 		byte stop =5;
+// 		while(start<=stop) {
+// 			System.out.println("PCPS COLLEGE");
+// 			start ++;
+// 		}
+// 		 System.out.println("pcps");
+// 		 System.out.println("pcps");
+// 		 System.out.println("pcps");
+// 		 System.out.println("pcps");
+// 		 System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps");System.out.println("pcps"
 
 
 
-	byte start=1;
-	byte stop=10;
-System.out.println("Enter any number: ");
-start = new Scanner(System.in).nextByte();3
-System.out.println("Enter any number: ");
-stop = new Scanner(System.in).nextByte();
+// 	byte start=1;
+// 	byte stop=10;
+// System.out.println("Enter any number: ");
+// start = new Scanner(System.in).nextByte();3
+// System.out.println("Enter any number: ");
+// stop = new Scanner(System.in).nextByte();
 
-while(start<=stop){
-	System.out.println(""+start);
-	start++;
+// while(start<=stop){
+// 	System.out.println(""+start);
+// 	start++;
 }
 }
-}
+
 
