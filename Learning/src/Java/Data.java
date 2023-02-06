5d8b9577c4634ec37e81f2288c864dbd4bd9f636
Your Name <you@example.com>
2023-02-06 14:34:30 +0545

    new

--- Learning/src/Java/Data.java
+++ Learning/src/Java/Data.java
@@ -3,11 +3,13 @@ package Java;
 public class Data {
 
 	public static void main(String[] args) {
-		
-//		Data types
-		float x=12;
-		char y=1;
-		System.out.println(y+x);
+
+		// Data types
+		float x = 12;
+		char y = 1;
+		System.out.println(y + x);
+		System.out.println(1 < 1);
+		System.out.println();
 
 	}
 
