de2db467ada26a024160965a433abf5baf8f22cd
Your Name <you@example.com>
2023-02-06 14:29:53 +0545

    new

--- Learning/src/Java/Integers.java
+++ Learning/src/Java/Integers.java
@@ -1,21 +1,17 @@
 package Java;
 
 public class Integers {
-	public static void main(String[] args) {
-        String x ="Ravi";
-        String y ="Shrestha";
-        String temp;
-//        Changing the value of integers
-      
-        temp=x;
-        x=y;
-        y=temp;
-        
-        System.out.println("value of x :" +x);
-        System.out.println("value of y :" +y);
-        
-        
-        
-      
-	}
+        public static void main(String[] args) {
+                String x = "Ravi";
+                String y = "Shrestha";
+                String temp;
+
+                temp = x;
+                x = y;
+                y = temp;
+
+                System.out.println("value of x :" + x);
+                System.out.println("value of y :" + y);
+
+        }
 }
